baf71e78e2d17e448b15b1f246fe033444e64079
Matt Wallace <mwallace@zerostatic.com>
2016-09-07 10:20:29 -0600

    firebase creds

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyDhlsB_ZUZvilYovMs5c4bLCgwj6M6xUwE",
-    authDomain: "react-router-auth.firebaseapp.com",
-    databaseURL: "https://react-router-auth.firebaseio.com",
-    storageBucket: "react-router-auth.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
   };
   
  const firebaseApp  = firebase.initializeApp(config);
